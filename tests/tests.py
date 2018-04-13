a40a92290824e74e2f7379f162351e069e675d11
Zhuo Zhen <zhenz@visitor099-231.wl.anl-external.org>
2018-04-13 15:01:15 -0500

    fix: always use tacc for cloudfuse test

--- tests/tests.py
+++ tests/tests.py
@@ -39,7 +39,7 @@ def test_cc_checks(server, shell):
     assert result.return_code == 0
     
 def test_cloudfuse(server, shell):
-    credentials = 'username={},password={},tenant={},authurl={}'.format(os.environ['OS_USERNAME'], os.environ['OS_PASSWORD'], os.environ['OS_TENANT_NAME'], os.environ['OS_AUTH_URL'])
+    credentials = 'username={},password={},tenant={},authurl={}'.format(os.environ['OS_USERNAME'], os.environ['OS_PASSWORD'], os.environ['OS_TENANT_NAME'], os.environ['OS_AUTH_URL'].replace('uc', 'tacc'))
     # Test the correct installation of cloudfuse
     result = shell.run(['cloudfuse', '-o', credentials, '-V'], allow_error=True, encoding='utf-8')
     assert 'fusermount version' in result.output
