8e26173354f462ffddad970b6bf6e6a55b87b7ee
Zhuo Zhen <zhenz@uchicago.edu>
2020-04-17 15:35:26 -0500

    add gnocchi cli test

--- tests/tests.py
+++ tests/tests.py
@@ -82,6 +82,11 @@ def test_provenance_data(server, shell, image):
     assert image['os'] == provenance_data['build-os']
     assert image['variant'] == provenance_data['build-variant']
 
+def test_gnocchi_cli(server, shell):
+    result = shell.run(['which', 'gnochhi'], encoding='utf-8')
+    assert result.return_code == 0
+    result = shell.run(['gnochhi', '--help'], encoding='utf-8')
+    assert result.return_code == 0
 
 @pytest.mark.require_os(['centos7', 'centos8', 'ubuntu-xenial', 'ubuntu-bionic']) # trusty cloud-init is too old
 def test_uids(server, shell):
